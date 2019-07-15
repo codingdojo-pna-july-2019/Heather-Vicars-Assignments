b9feaa3329ce386b607b1a0d5677fae7f880d7d2
heathervicars <heathervicars@yahoo.com>
2019-07-15 13:44:22 -0700

    completed length

--- singlylinkedlists.js
+++ singlylinkedlists.js
@@ -15,6 +15,11 @@ Rudy isn’t nice: he cuts in line in front of everyone else. Given a pointer to
 // Front
 // Finally, Tad and Sam reach the front of the line to get movie tickets. Oh no – only one seat remains! Who was earlier in line: Tad or Sam? Return the value (not the node) at the head of the list. If the list is empty, return null.
 
+//Contains
+
+// Length
+// July 20, 2013: about 5000 people wait in line for a chance to audition for American Idol. Create a function that accepts a pointer to the first list node, and returns number of nodes in that SList.
+
 class Node {
     constructor(value) {
         this.value = value;
@@ -97,6 +102,20 @@ class List {
 
         return false;
     }
+
+    length() {
+        console.log("Beginning to count the list");
+        let current = this.head;
+        let lengthIs = 0;
+        while (current) {
+            current = current.next;
+            lengthIs++;
+        }
+        
+        console.log(lengthIs);
+        console.log("finished counting the list");
+        return lengthIs;
+    }
 }
 
 const array = [1,2,3,4,5,6,7];
@@ -117,5 +136,6 @@ console.log(list.contains(7));
 list.print();
 
 console.log(list.front(9,11));
+console.log(list.length());
 
 
